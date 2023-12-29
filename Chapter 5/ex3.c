0ce9fefb032e13c281f2b356d88d66e9eadedd89
Ajay-Kumaar <ajay@mobicip.com>
2023-12-29 18:40:06 +0530

    Added ex 6 to ex 10

--- Chapter 5/ex3.c
+++ Chapter 5/ex3.c
@@ -1,3 +1,6 @@
+/*Exercise 5-3. Write a pointer version of the function strcat that we showed in Chapter 2:
+strcat(s,t) copies the string t to the end of s .*/
+
 #include<stdio.h>
 #define MAXLINE 1000
 
--- Chapter 5/ex4.c
+++ Chapter 5/ex4.c
@@ -1,3 +1,6 @@
+/*Exercise 5-4. Write the function strend(s,t) , which returns 1 if the string t occurs at the
+end of the string s , and zero otherwise.*/
+
 #include<stdio.h>
 #define MAXLINE 1000
 
